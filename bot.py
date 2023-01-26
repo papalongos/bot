bc0c7faef13e278dc77580035c2f1b2e5de78016
papalongos <123345144+papalongos@users.noreply.github.com>
2023-01-25 21:10:00 -0800

    Load an order book

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
+import json
 import ccxt
 
-print(ccxt.exchanges)
-# kucoin = ccxt.kucoin()
+#print(ccxt.exchanges)
+
+kucoin = ccxt.kucoin()
+markets = kucoin.load_markets()
+
+order_book_symbol = kucoin.symbols[0]
+
+order_book = kucoin.fetch_order_book(kucoin.symbols[0])
+
+print(json.dumps(order_book, indent=2))
+
+# print(order_book)
 # print(kucoin.fetch_order_book())
